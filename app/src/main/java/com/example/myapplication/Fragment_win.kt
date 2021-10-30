65e4dfc17d9da1cf9a266747676e503a304a9a59
ndg5778 <59169163+ndg5778@users.noreply.github.com>
2021-10-31 03:54:29 +0900

    후회하기 전

--- app/src/main/java/com/example/myapplication/Fragment_win.kt
+++ app/src/main/java/com/example/myapplication/Fragment_win.kt
@@ -1,21 +1,72 @@
 package com.example.myapplication
 
 import android.content.Context
-import android.nfc.Tag
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import kotlin.math.log
 
 data class ListViewItem(val title: String, val local: String, val date: String, val cost: Int)
+
+class win_list internal constructor(
+    var title: String,
+    var content: String,
+    var location: String,
+    var score: String
+)
+
+//class ListAdapter(_oData: java.util.ArrayList<win_list>?) : BaseAdapter() {
+//    var inflater: LayoutInflater? = null
+//    private var m_oData: java.util.ArrayList<win_list>? = null
+//    private var nListCnt = 0
+//    override fun getCount(): Int {
+//        Log.i("TAG", "GETCOUNT")
+//        return nListCnt
+//    }
+//
+//    override fun getItem(position: Int): Any? {
+//        return null
+//    }
+//
+//    override fun getItemId(position: Int): Long {
+//        return 0
+//    }
+//
+//    override fun getView(position: Int, convertView: View, parent: ViewGroup): View {
+//        var convertView = convertView
+//        if (convertView == null) {
+//            val context = parent.context
+//            if (inflater == null) {
+//                inflater =
+//                    context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+//            }
+//            convertView = inflater.inflate(R.layout.item2, parent, false)
+//        }
+//        val oTourName = convertView.findViewById<View>(R.id.titlewin) as TextView
+//        val oTourContent = convertView.findViewById<View>(R.id.tour_content) as TextView
+//        val oTourLocation = convertView.findViewById<View>(R.id.tour_loation) as TextView
+//        val oTourScore = convertView.findViewById<View>(R.id.tour_score) as TextView
+//        oTourName.setText(m_oData!![position].title)
+//        oTourContent.setText(m_oData!![position].content)
+//        oTourLocation.setText(m_oData!![position].location)
+//        oTourScore.setText(m_oData!![position].score)
+//        return convertView
+//    }
+//
+//    init {
+//        m_oData = _oData
+//        nListCnt = m_oData!!.size
+//    }
+//}
 
 class Fragment_win : Fragment() {
     companion object {
@@ -36,6 +87,19 @@ class Fragment_win : Fragment() {
         myRef.addValueEventListener(object: ValueEventListener {
 
             override fun onDataChange(snapshot: DataSnapshot) {
+
+                val titlewin: Array<String> = arrayOf("[국비]한식조리기능사양성과정(온라인)", "[교육서비스] 아이돌보미 온라인 보수교육 4회기(청양)")
+                val datewin: Array<String> = arrayOf("now", "now")
+                val localwin: Array<String> = arrayOf("online", "충청남도")
+                val costwin: Array<String> = arrayOf("0", "18000")
+
+                var oData : ArrayList<win_list> = ArrayList()
+
+                for(i : Int in 0..1){
+                    val win = win_list(titlewin[i], datewin[i],localwin[i],costwin[i])
+                    oData.add(win)
+                }
+
 //                val value = snapshot.value
 //                val date = snapshot.child("0").child("date").value
 //                Log.d(TAG, "Value is: $value")
--- app/src/main/java/com/example/myapplication/win_list.kt
+++ app/src/main/java/com/example/myapplication/win_list.kt
@@ -1,5 +1,55 @@
 package com.example.myapplication
 
-class win_list (val profile: Int, val name: String, val age: String, val greet: String){
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import java.util.ArrayList
 
+class ListAdapter(_oData: ArrayList<win_list>?) : BaseAdapter() {
+    var inflater: LayoutInflater? = null
+    private var m_oData: ArrayList<win_list>? = null
+    private var nListCnt = 0
+    override fun getCount(): Int {
+        Log.i("TAG", "GETCOUNT")
+        return nListCnt
+    }
+
+    override fun getItem(position: Int): Any? {
+        return null
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getView(position: Int, convertView: View, parent: ViewGroup): View {
+        var convertView = convertView
+        if (convertView == null) {
+            val context = parent.context
+            if (inflater == null) {
+                inflater =
+                    context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            }
+            convertView = inflater!!.inflate(R.layout.fragment_win, parent, false)
+        }
+        val oTourName = convertView.findViewById<View>(R.id.title_win) as TextView
+        val oTourContent = convertView.findViewById<View>(R.id.now_win) as TextView
+        val oTourLocation = convertView.findViewById<View>(R.id.local_win) as TextView
+        val oTourScore = convertView.findViewById<View>(R.id.price_win) as TextView
+        oTourName.setText(m_oData!![position].title)
+        oTourContent.setText(m_oData!![position].content)
+        oTourLocation.setText(m_oData!![position].location)
+        oTourScore.setText(m_oData!![position].score)
+        return convertView
+    }
+
+    init {
+        m_oData = _oData
+        nListCnt = m_oData!!.size
+    }
 }
